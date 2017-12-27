aa6b9d312f7e59fefe50c3be567992b0d06cc302
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-12-27 22:40:47 +0100

    disable image smoothing for pixel look

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ function main()
 // const scale = Math.min(window.innerWidth/canvas.width,window.innerHeight/canvas.height);
 // canvas.setAttribute('style','transform: scale('+scale+')');//removes existing style
  ctx = canvas.getContext("2d");
+ ctx.mozImageSmoothingEnabled = false;
+ ctx.webkitImageSmoothingEnabled = false;
  console.log();
  state = new GameState(canvas,ctx);
  mainLoop(time());
