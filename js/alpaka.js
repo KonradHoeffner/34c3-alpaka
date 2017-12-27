c06d0c95a4f119347c15b03903ed31096f2665ae
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-12-27 21:45:50 +0100

    add sprite class

--- js/alpaka.js
+++ js/alpaka.js
@@ -1,10 +1,11 @@
 const GRAVITY = 1.0;
+const alpakaSprite = new Sprite("./img/background_01.jpg",20,20);
 
 class Alpaka
 {
- constructor()
+ constructor(x)
  {
-  this.x=0;
+  this.x=x;
   this.y=0;
   this.vx=0;
   this.vy=0;
@@ -23,4 +24,8 @@ class Alpaka
   //if(vy<0) {vy=0;}
  }
 
+ draw()
+ {
+
+ }
 }
