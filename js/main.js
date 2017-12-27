1c8b9b46158c3db41f8ee99f07f30693f1293773
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-12-27 22:39:48 +0100

    less cpu usage

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ let canvas;
 let ctx;
 let timestamp = 0;
 let now = time();
-let last;
+let last=time();
 let state;
+const MIN_TIME=1000.0/10;
 
 function main()
 {
@@ -12,17 +13,30 @@ function main()
 // const scale = Math.min(window.innerWidth/canvas.width,window.innerHeight/canvas.height);
 // canvas.setAttribute('style','transform: scale('+scale+')');//removes existing style
  ctx = canvas.getContext("2d");
- state = new GameState(canvas);
+ console.log();
+ state = new GameState(canvas,ctx);
  mainLoop(time());
 }
 
 
 function mainLoop()
 {
- last = now;
- now = time();
- state.update(now-last);
- state.draw();
+ if((time()-last)>MIN_TIME)
+ {
+  last = now;
+  now = time();
+  state.update(now-last);
+  state.draw();
+ } else
+ {
+  setTimeout(wait,Math.min(10,MIN_TIME-(time()-last)+1));
+  return;
+ }
+ requestAnimationFrame(mainLoop);
+}
+
+function wait()
+{
  requestAnimationFrame(mainLoop);
 }
 
