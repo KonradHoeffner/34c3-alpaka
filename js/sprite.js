73e639a31ee499346e2811a5adabf8e2d4bbb2bb
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-12-27 22:49:07 +0100

    fix sprite drawing

--- js/sprite.js
+++ js/sprite.js
@@ -2,14 +2,15 @@ class Sprite
 {
  constructor(img, width, height)
  {
-   this.img = img;
+   this.img = new Image();
+   this.img.src=img;
    this.width = width;
    this.height = height;
  }
 
  draw(ctx, x, y)
  {
-  ctx.drawImage(this.img,x, y);
+  ctx.drawImage(this.img,x,y, this.width,this.height);
  }
 
 }
