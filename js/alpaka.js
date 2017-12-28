d4fd6ef7b44fe3babd6c9aca322f7f5a16a9823f
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2017-12-28 20:30:42 +0100

    increase slow speed for crouch animation threshold

--- js/alpaka.js
+++ js/alpaka.js
@@ -1,6 +1,6 @@
 const GRAVITY = 0.00015;
 const VY_MAX = 2;
-const VY_SLOW = 0.1;
+const VY_SLOW = 0.3;
 const SCALE = 4;
 const spriteUris =
 [
@@ -28,7 +28,7 @@ class Alpaka
   this.vy+=GRAVITY*dt;
   this.vy=Math.min(this.vy,VY_MAX);
   this.y+=this.vy*dt;
-  
+    
   if(this.y>780)
   {
    state.lost=true;
@@ -44,12 +44,17 @@ class Alpaka
   if(this.x<0&&this.vx<0) {this.vx=-this.vx;}
   if(this.x>1200&&this.vx>0) {this.vx=-this.vx;}
   // in ship?
-  if(this.vy<0&&this.y<50&&this.x>400&&this.x<840)
+  if(this.vy<0&&this.y<50)
   {
+   if(this.x>400&&this.x<840)
+   {
    state.addPoints(1);
    this.isAlive=false;
+   } else
+   {
+    this.vy
+   }
   }
-
  }
 
  draw(ctx)
